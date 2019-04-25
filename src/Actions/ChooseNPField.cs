fbea0acc7c14944feb794986b7de0b855511eeb3
Jenna Solis <jennabsol@gmail.com>
2019-04-24 19:49:20 -0500

    sunflower can now be added to natural or plowed flieds

--- src/Actions/ChooseNPField.cs
+++ src/Actions/ChooseNPField.cs
@@ -13,22 +13,37 @@ namespace Trestlebridge.Actions
         {
             Console.Clear();
 
-            List<Dictionary<int, ICompost_Seed>> Fields = new List<Dictionary<int, ICompost_Seed>>();
+            List<KeyValuePair<int, ICompost_Seed>> Fields = new List<KeyValuePair<int, ICompost_Seed>>();
 
             for (int i = 0; i < farm.NaturalFields.Count; i++)
             {
-                Console.WriteLine($"{i + 1}. Natural Field {farm.NaturalFields[i].getCount()}");
-Dictionary<int, ICompost_Seed> Nplant = new Dictionary<int, ICompost_Seed>(){};
-Nplant.Add(i, farm.NaturalFields[i]);
-                Fields.Add(Nplant);
+                // Console.WriteLine($"{i + 1}. Natural Field {farm.NaturalFields[i].getCount()}");
+                Fields.Add(new KeyValuePair<int, ICompost_Seed>(i, farm.NaturalFields[i]));
             }
 
             for (int i = 0; i < farm.PlowedFields.Count; i++)
             {
-                Console.WriteLine($"{i + 1}. Plowed Field {farm.PlowedFields[i].getCount()}");
-                Dictionary<int, ICompost_Seed> Nplant = new Dictionary<int, ICompost_Seed>() { };
-                Nplant.Add(i, farm.NaturalFields[i]);
-                Fields.Add(Nplant);
+                // Console.WriteLine($"{i + 1}. Plowed Field {farm.PlowedFields[i].getCount()}");
+                Fields.Add(new KeyValuePair<int, ICompost_Seed>(i, farm.PlowedFields[i]));
+            }
+
+            for (int i = 0; i < Fields.Count; i++)
+            {
+
+                {
+                   
+                    switch (Fields[i].Value.GetType().ToString())
+                    {
+                        case "Trestlebridge.Models.Facilities.NaturalField":
+                            Console.WriteLine($"{i + 1}. Natural Field {Fields[i].Value.getCount()}");
+                            break;
+                        case "Trestlebridge.Models.Facilities.PlowedField":
+                            Console.WriteLine($"{i + 1}. Plowed Field {Fields[i].Value.getCount()}");
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
 
             Console.WriteLine();
@@ -39,24 +54,39 @@ Nplant.Add(i, farm.NaturalFields[i]);
             Console.Write("> ");
             int choice = Int32.Parse(Console.ReadLine());
             choice = choice - 1;
-            // if (!farm.NaturalFields[choice].AddResource(plant))
-            // {
-            //     Console.WriteLine($"Natural Field {choice + 1} is at max capacity!");
-            //     Console.ReadLine();
-            //     ChooseNaturalField.CollectInput(farm, plant);
-            // }
-            // else
-            // {
-            //     Console.WriteLine($"You added {plant} to Natural Field {choice + 1}!");
-            //     Console.ReadLine();
-            // }
 
-            /*
-                Couldn't get this to work. Can you?
-                Stretch goal. Only if the app is fully functional.
-             */
-            // farm.PurchaseResource<INatural>(animal, choice);
-
+            // foreach (KeyValuePair<int, ICompost_Seed> obj in Fields[choice])
+            switch (Fields[choice].Value.GetType().ToString())
+            {
+                case "Trestlebridge.Models.Facilities.NaturalField":
+                    if (!farm.NaturalFields[Fields[choice].Key].AddResource((ICompostable)plant))
+                    {
+                        Console.WriteLine($"Natural Field {choice + 1} is at max capacity!");
+                        Console.ReadLine();
+                        ChooseNaturalField.CollectInput(farm, (ICompostable)plant);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You added {plant} to Natural Field {choice + 1}!");
+                        Console.ReadLine();
+                    }
+                    break;
+                case "Trestlebridge.Models.Facilities.PlowedField":
+                    if (!farm.PlowedFields[Fields[choice].Key].AddResource((ISeedProducing)plant))
+                    {
+                        Console.WriteLine($"Plowed Field {choice + 1} is at max capacity!");
+                        Console.ReadLine();
+                        ChoosePlowedField.CollectInput(farm, (ISeedProducing)plant);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You added {plant} to plowed Field {choice + 1}!");
+                        Console.ReadLine();
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
